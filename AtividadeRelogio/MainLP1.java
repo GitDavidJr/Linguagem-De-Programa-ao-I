b3918c20af75adecbab5d67990a364a0a9c605a8
David Júnior <davidjunior051204@gmail.com>
2024-08-07 23:59:05 -0300

    Relogio Commit

---
+++ AtividadeRelogio/MainLP1.java
@@ -0,0 +1,12 @@
+package AtividadeRelogio;
+
+public class MainLP1 {
+    public static void main(String[] args) {
+        
+        RelogioLP1 r1 = new RelogioLP1(12, 30, 00);
+
+        r1.setHora(25, 70, 95);
+
+        r1.getHora();
+    }
+}
